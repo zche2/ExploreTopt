4913123bff0c7bfd7208ffa9d814b56fb0cb727a
Zichang He <zche233@gmail.com>
2025-05-27 15:03:30 -0700

    Add files via upload

---
+++ ToptCalculation_202301.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+def findbin(x):
+    y = (math.ceil(x)+math.floor(x))/2.
+    # y = round(x)
+    return y
+
+def running_mean3(ARRAY):
+    L = len(ARRAY)
+    TRG = np.zeros(L)*np.nan
+    for i in range(L):
+        if i==0:
+            TRG[i] = np.nanmean(ARRAY[i:i+2])
+        elif i==L-1:
+            TRG[i] = np.nanmean(ARRAY[i-1:i+1])
+        else:
+            TRG[i] = np.nanmean(ARRAY[i-1:i+2])
+    return TRG
+
+def find90(INPUT):
+    ARRAY = np.array(INPUT)
+    if np.nansum(ARRAY==ARRAY)==0:
+        return np.nan
+    L = len(ARRAY)
+    ARRAY.sort()
+    ORDER = np.arange(0,L,1)+0.5
+    ORDER = 100*ORDER/L
+    # print('order is:',ORDER)
+    FIND = np.argwhere(ORDER==90)
+    if FIND.shape[0]!=0:
+        i = FIND[0,0]
+        return ARRAY[i]
+    FIND = np.argwhere(ORDER<90)
+    i = FIND[-1,0]
+    if i==L-1:
+        return ARRAY[-1]
+    j = i+1
+    X = ARRAY[i]+(90-ORDER[i])*(ARRAY[j]-ARRAY[i])/(ORDER[j]-ORDER[i])
+    return X
+
+def find_opt_T(T,VI,flag=0,VALID_YR=[0], YR=0):
+    ## if Topt can be derived
+    if YR not in VALID_YR:
+        return np.nan
+    ## choose valid data
+    if np.nansum(VI==VI)==np.nansum(VI<=0):     #for FLUXNET gpp！
+        return np.nan
+    elif np.nansum(T==T)==0:
+        return np.nan
+    else:
+        valid = np.logical_and(T==T, VI==VI)
+        if ~np.any(valid):
+            return np.nan
+        T_tmp = T[valid]
+        VI_tmp = VI[valid]
+
+        tt = [findbin(i) for i in T_tmp]
+        t_array = np.unique(tt)
+        t_array.sort()
+        L = len(t_array)
+        t_bin = np.zeros(L)*np.nan
+        vi_bin = np.zeros(L)*np.nan
+        for i in range(L):
+            valid = tt==t_array[i]
+            t_bin[i] = np.nanmean(T_tmp[valid])     ### 更新：bin内求平均
+            vi90 = np.nanmax(VI_tmp[valid])     # for 模拟的GPP 100%
+            vi_bin[i] = vi90
+        # 12月11日更改
+        if L>5:
+            trg = running_mean3(vi_bin)
+        else:
+            trg = vi_bin
+        index = np.nanargmax(trg)
+
+        if flag==1:
+            fig=plt.figure(figsize=(5,5),dpi=150)
+            plt.scatter(T_tmp, VI_tmp, s = 30, c='silver')
+            plt.plot(t_bin, trg,'k-',linewidth=3)
+            plt.scatter(t_bin, vi_bin,c='coral',s = 60, zorder=3)
+            global SITE_NAME
+            plt.title("%s %d"%(SITE_NAME,YR), fontsize=20)
+            plt.xticks(fontsize= 15)
+            plt.yticks(fontsize= 15)
+            ax=plt.gca();#获得坐标轴的句柄
+            ax.spines['bottom'].set_linewidth(1.5);###设置底部坐标轴的粗细
+            ax.spines['left'].set_linewidth(1.5);####设置左边坐标轴的粗细
+            ax.spines['right'].set_linewidth(1.5);###设置右边坐标轴的粗细
+            ax.spines['top'].set_linewidth(1.5);####设置上部坐标轴的粗细
+            plt.show()
+            # fig.savefig('/Users/hzc/data/LP/pic/%s %s.png'%(SITE_NAME,pic_tt))#;plt.close(fig)
+
+        # if index == len(trg)-1 or index==0:
+        # 12月11日更改
+        if index == len(trg)-1:
+            return np.nan
+        else:
+            return t_bin[index].round(1)
+
---
+++ derive_Topt_model.py
@@ -0,0 +1,69 @@
+import sys
+sys.path.append('/workdir/hezichang/pycode/pyTools')
+
+import json
+import pandas as pd
+import numpy as np
+import os
+import csv
+from ToptCalculation_202301 import find_opt_T
+"""
+Update: 2025/01/12
+change Topt detection criteria and create json file to automize the calculation.
+
+Date: 2024/12/09
+描述：把服务器上已做过QC、计算好的GPP-model值的各个站点表格汇总计算Topt，输出文件在Topt_model的文件夹中
+update: label ~valid_yr and undetectable Topt as -9999 and np.nan, respectively.
+"""
+
+with open("/workdir/hezichang/pycode/MoreAnalysis_2024/json_deriveTopt.txt","r", encoding='UTF-8') as f:
+    mydict = json.load(f)
+
+path      = mydict["path"]
+SAVE_NAME = mydict["SAVE_NAME"]
+
+allpath=os.listdir(path)
+allpath.sort()
+
+# prepare a year list
+all_yr = list(np.arange(1992,2022))
+# 列表拼接的方法：
+final = [["site"]+all_yr]
+
+for ipath in allpath:
+    SITE_NAME = ipath[0:6]
+    FILE_NAME = path+'/'+ipath
+    # read csv, already QC
+    site = pd.read_csv(FILE_NAME, index_col=0) 
+    # record valid yr
+    valid_yr = site["year"].unique()
+    print(valid_yr, type(valid_yr))
+    # aggregate into ymd, cal Tdmax and GPP_acc
+    site.loc[site["GPP_model"]<0,'GPP_model'] = np.nan
+    site = site[["year","yearmonth","ymd","TA_F","GPP_model"]].groupby(["ymd"]).agg({
+        "TA_F":np.max,"year":np.mean, "yearmonth":np.mean,"GPP_model":np.sum}).reset_index()
+
+    # cal Topt
+    if len(valid_yr)==0:
+        Topt = np.zeros(30) - 9999
+    else:
+        print(SITE_NAME)
+        Topt = np.zeros(len(all_yr)) - 9999
+        for i,yr in enumerate(all_yr):
+            if yr not in valid_yr:
+                continue
+            else:
+                print(i,yr)
+                subset = site.loc[site['year']==yr]
+                Topt[i] = find_opt_T(subset['TA_F'], subset['GPP_model'],0, valid_yr, yr)
+    
+    Topt = Topt.tolist()
+    final.append([SITE_NAME]+Topt)
+
+# save as new csv
+for i in range(len(final)):
+    with open(SAVE_NAME,'a+', newline='') as f:      
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(final[i])
+
+
